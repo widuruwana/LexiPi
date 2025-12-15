f0c00d5914b7997cc1ea7845a1b1a141ae63bddd
Kavindu Widyarathne <kavinduwidya@gmail.com>
2025-12-15 21:26:39 +0530

    Have to rewrite ai_player.h, found a mismatch

---
+++ include/ai_player.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "player_controller.h"
+#include "dawg.h"
+#include "fast_constraints.h"
+#include "rack.h"
+#include <vector>
+#include <string>
+
+//using namespace std;
+
+struct MoveCandidate {
+    int row, col;
+    string word;
+    int score;
+    bool isHorizontal;
+};
+
+class AIPlayer : public PlayerController {
+public:
+    AIPlayer(string name) : PlayerController(name) {};
+
+    // Main entry point called by the game loop
+    Move getMove(const Board &board, const TileRack &rack) override;
+
+private:
+    vector <MoveCandidate> candidates;
+
+    // The solver functions
+    void findAllMoves(const Board& board, const TileRack &rack);
+
+    void solveRow(int rowIdx, const LetterBoard &letters, const TileRack &rack, bool isHorizontal);
+
+    // Recursive "Tunneling" function
+    // nodeIdx: Current node in DAWG
+    // col: Current column on board
+    // currentWord: The word built so far
+    // tilesUsed: which tiles from rack we used (to avoid re-using)
+    // anchorFilled: Have we connected to existing board yet?
+    void recursiveSearch(int nodeIdx,
+                         int col,
+                         const RowConstraint &constraints,
+                         string currentWord,
+                         string currentRack,
+                         bool anchorFilled,
+                         const LetterBoard &letters);
+};
---
+++ src/ai_player.cpp
@@ -0,0 +1,10 @@
+#include "../include/ai_player.h"
+#include "../include/heuristics.h" // For scoring
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+Move AIPlayer::getMove(const Board &board, const Rack &rack) {
+
+}
